d55e30cbdf25303f0eb2dcda5a35afa6164bd3ae
Paul Graham <pgraham@hlkagency.com>
2016-07-14 13:42:12 -0500

    partial menu functionality

---
+++ wp-content/themes/connectIN/scripts/menu.js
@@ -0,0 +1,18 @@
+$('#menu-activate').on('click touchend', function () {
+  let mailbar = 0
+  if ($('#mailbar-body').length) {
+    mailbar = $('#mailbar').height()
+  }
+
+  const vh = $(window).height() - $('#menu').height() - mailbar
+  const menu = '<use xlink:href="#icon-menu"></use>'
+  const cross = '<use xlink:href="#icon-cross"></use>'
+
+  if ($('#menu-header-menu-container').height() === 0) {
+    $('#menu-header-menu-container').animate({height: vh })
+    $(this).children('svg').html(cross)
+  } else {
+    $('#menu-header-menu-container').animate({ height: 0 })
+    $(this).children('svg').html(menu)
+  }
+})
