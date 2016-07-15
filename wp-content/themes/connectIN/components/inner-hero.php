fd32a511c0266707e7d3d487b89f9299caae347d
Paul Graham <pgraham@hlkagency.com>
2016-07-15 15:43:12 -0500

    Seed Supplier page mostly done on mobile (still need Nexa font for buttons)

---
+++ wp-content/themes/connectIN/components/inner-hero.php
@@ -0,0 +1,13 @@
+<div class="inner-hero">
+  <h1 class="inner-hero-headline"><?php the_title(); ?></h1>
+  <div class="inner-hero-copy">
+    <?php the_field('hero_copy'); ?>
+  </div>
+</div>
+
+<div class="seed-supplier-cta">
+  <p><strong>Find seed suppliers and WestBred representatives at WestBred.com.</strong></p>
+  <p>
+    <a class="button" href="http://www.westbred.com/associates/pages/locateassociates.aspx">Search Now</a>
+  </p>
+</div>
