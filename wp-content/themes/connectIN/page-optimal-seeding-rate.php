0e2ae706225ab58ca82ecd68f597e75d05d3cafd
Omar Rasool <orasool@hlkagency.com>
2016-07-20 17:10:20 -0500

    adds flexboxgrid to optimal seeding rate page and styles

--- wp-content/themes/connectIN/page-optimal-seeding-rate.php
+++ wp-content/themes/connectIN/page-optimal-seeding-rate.php
@@ -2,17 +2,29 @@
 get_header();
 get_template_part('components/inner-hero'); ?>
 
-<div class="risks">
-  <h2><?php the_field('risks_headline'); ?></h2>
-  <div class="risks-copy"><?php the_field('risks_copy'); ?></div>
-  <div class="risks-seeding">
-    <div class="risks-underseed">
-      <img src="<?php the_field('underseed_image'); ?>">
-      <div class="risks-underseed-copy"><?php the_field('underseed_copy'); ?></div>
+<div class="risks row">
+  <div class="col-xs-12">
+    <div class="box center">
+      <h2><?php the_field('risks_headline'); ?></h2>
+      <div class="risks-copy"><?php the_field('risks_copy'); ?></div>
     </div>
-    <div class="risks-overseed">
-      <img src="<?php the_field('overseed_image'); ?>">
-      <div class="risks-underseed-copy"><?php the_field('overseed_copy'); ?></div>
+  </div>
+
+  <div class="risks-seeding room row">
+    <div class="col-sm-6">
+      <div class="box">
+        <div class="risks-underseed">
+          <img src="<?php the_field('underseed_image'); ?>">
+          <div class="risks-underseed-copy"><?php the_field('underseed_copy'); ?></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6">
+      <div class="box">
+        <img src="<?php the_field('overseed_image'); ?>">
+        <div class="risks-underseed-copy"><?php the_field('overseed_copy'); ?></div>
+      </div>
     </div>
   </div>
 </div>
