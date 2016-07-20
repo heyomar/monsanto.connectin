6f7154b492956f2ce7cc83c81137c78c834956a2
Omar Rasool <orasool@hlkagency.com>
2016-07-20 17:11:14 -0500

    adds flexboxgrid to about the system page and styles

--- wp-content/themes/connectIN/page-about-the-system.php
+++ wp-content/themes/connectIN/page-about-the-system.php
@@ -2,15 +2,23 @@
       get_template_part('components/inner-hero');?>
 
   <!-- THE MEAT -->
-  <div class="why__ctn">
-    <h3 class="headline"><?php the_field('why_headline'); ?></h3>
-    <?php the_field('why_copy'); ?>
-    <div class="why__video"><?php the_field('why_video'); ?></div>
+  <div class="why__ctn row">
+    <div class="col-sm-12">
+      <div class="box room">
+        <h3 class="headline"><?php the_field('why_headline'); ?></h3>
+        <?php the_field('why_copy'); ?>
+        <div class="why__video center"><?php the_field('why_video'); ?></div>
+      </div>
+    </div>
   </div>
 
-  <div class="benefits__ctn">
-    <h3 class="headline"><?php the_field('benefits_headline'); ?></h3>
-    <?php the_field('benefits_copy'); ?>
+  <div class="benefits__ctn row">
+    <div class="col-xs-12">
+      <div class="box">
+        <h3 class="headline"><?php the_field('benefits_headline'); ?></h3>
+        <?php the_field('benefits_copy'); ?>
+      </div>
+    </div>
   </div>
 
 
