726043f5095c05d4ba46966b37ad083e6ce263a4
Omar Rasool <orasool@hlkagency.com>
2016-07-20 15:18:32 -0500

    adds home page layout and grid

--- wp-content/themes/connectIN/page-home.php
+++ wp-content/themes/connectIN/page-home.php
@@ -10,31 +10,39 @@
     </div>
   </div>
 
-  <div class="seed__ctn flex__ctn">
-    <div class="flex__three">
-      <h3 class="seed__headline headline"><?php the_field('seed_headline'); ?></h3>
-      <?php the_field('seed_copy'); ?>
-      <a href="" class="seed__button button__plain"><?php the_field('seed_button_copy'); ?></a>
+  <div class="seed__ctn row">
+    <div class="col-sm-8">
+      <div class="box room">
+        <h3 class="seed__headline headline"><?php the_field('seed_headline'); ?></h3>
+        <?php the_field('seed_copy'); ?>
+        <a href="" class="seed__button button__plain"><?php the_field('seed_button_copy'); ?></a>
+      </div>
     </div>
 
-    <div class="flex__one">
-      <img src="http://placehold.it/350x150" alt="" />
+    <div class="col-sm-4">
+      <div class="box room">
+        <img class="seed__image" src="<?php the_field('seed_image'); ?>" alt="" />
+      </div>
     </div>
   </div>
 
-  <div class="flex__ctn">
-    <div class="supply__ctn flex__half">
-      <h3 class="supply__headline headline"><?php the_field('supply_headline'); ?></h3>
-      <?php the_field('supply_copy'); ?>
-      <a href="" class="supply__button button__plain"><?php the_field('supply_button_copy'); ?></a>
+  <div class="row">
+    <div class="supply__ctn col-sm-6">
+      <div class="box room">
+        <h3 class="supply__headline headline"><?php the_field('supply_headline'); ?></h3>
+        <?php the_field('supply_copy'); ?>
+        <a href="" class="supply__button button__plain"><?php the_field('supply_button_copy'); ?></a>
+      </div>
     </div>
 
-    <div class="calc__ctn flex__half">
-      <h3 class="calc__headline headline"><?php the_field('calc_headline'); ?></h3>
-      <?php the_field('calc_copy'); ?>
-      <a href="" class="calc__button button__plain"><?php the_field('calc_button_copy'); ?></a>
+    <div class="calc__ctn col-sm-6">
+      <div class="box room">
+        <h3 class="calc__headline headline"><?php the_field('calc_headline'); ?></h3>
+        <?php the_field('calc_copy'); ?>
+        <a href="" class="calc__button button__plain"><?php the_field('calc_button_copy'); ?></a>
+      </div>
     </div>
-  <div>
+  </div>
 
 
 <?php get_footer(); ?>
