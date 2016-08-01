e29f40637e9691cf68a990ed95587ac6851fcf7e
Omar Rasool <orasool@hlkagency.com>
2016-08-01 15:53:47 -0500

    opens CTA in new tab

--- wp-content/themes/connectIN/components/seed-supplier-cta.php
+++ wp-content/themes/connectIN/components/seed-supplier-cta.php
@@ -1,6 +1,6 @@
 <div class="seed-supplier-cta">
   <p><strong>Find seed suppliers and WestBred representatives at WestBred.com.</strong></p>
   <p>
-    <a class="button" href="http://www.westbred.com/associates/pages/locateassociates.aspx">Search Now</a>
+    <a target="_blank" class="button" href="http://www.westbred.com/associates/pages/locateassociates.aspx">Search Now</a>
   </p>
 </div>
