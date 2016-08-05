ba6253c1e22044e06becf94bd18bb41a43c90540
Omar Rasool <orasool@hlkagency.com>
2016-08-05 14:47:35 -0500

    adds contact us thank you

---
+++ wp-content/themes/connectIN/page-thank-you-contact.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="inner-hero">
+  <h1 class="inner-hero-headline">Thank You</h1>
+  <div class="inner-hero-copy">
+    <?php the_field('hero_copy'); ?>
+  </div>
+
+</div>
+<div class="seed-supplier-cta">
+  <p><strong>In the meantime, find a seed supplier or<br>WestBred representative near you.</strong></p>
+  <p>
+    <a target="_blank" class="button" href="http://www.westbred.com/associates/pages/locateassociates.aspx">Find a Seed Supplier</a>
+  </p>
+</div>
+<?php get_footer();?>
