fff2f61aac89025511fbd216ef8f0f5a4d7cd258
Omar Rasool <orasool@hlkagency.com>
2016-08-08 10:42:31 -0500

    tests js

--- wp-content/themes/connectIN/page-thank-you.php
+++ wp-content/themes/connectIN/page-thank-you.php
@@ -1,12 +1,6 @@
 <?php
 get_header();
 get_template_part('components/inner-hero');?>
-<script type="text/javascript">
-if(window.location.href === "http://<?php echo $WP_SITEURL; ?>/thank-you/") {
-    document.cookie = 'subscribed=true; expires=Fri, 31 Dec 9999 23:59:59 GMT'
-    alert("Cookie Set!")
-}
-</script>
 <div class="seed-supplier-cta">
   <p><strong>In the meantime, find a seed supplier or<br>WestBred representative near you.</strong></p>
   <p>
