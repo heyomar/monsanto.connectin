f39b04de5b9783d27993a681429a06f469854819
Omar Rasool <orasool@hlkagency.com>
2016-08-19 16:25:39 -0500

    adds wp query to get all suppliers, adds 50 states dropdown

--- wp-content/themes/connectIN/page-find-seed-supplier.php
+++ wp-content/themes/connectIN/page-find-seed-supplier.php
@@ -1,6 +1,107 @@
-<?php
-get_header();
-get_template_part('components/inner-hero');
-get_template_part('components/seed-supplier-cta');
-get_footer();
-?>
+<?php get_header(); ?>
+
+<div class="row">
+  <div class="col-xs-12 col-sm-8">
+    <div class="box">
+      <div class="inner-hero">
+          <h1 class="inner-hero-headline"><?php the_title(); ?></h1>
+          <div class="inner-hero-copy">
+            <?php the_field('hero_copy'); ?>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-xs-12 col-sm-4">
+    <div class="box">
+        <div class="suppliers__select-ctn">
+          <p>Find seed suppliers and WestBred representatives near you.</p>
+
+            <label>
+              <select class="" name="">
+                <option value="">Select a state</option>
+                <option value="AL">Alabama</option>
+              	<option value="AK">Alaska</option>
+              	<option value="AZ">Arizona</option>
+              	<option value="AR">Arkansas</option>
+              	<option value="CA">California</option>
+              	<option value="CO">Colorado</option>
+              	<option value="CT">Connecticut</option>
+              	<option value="DE">Delaware</option>
+              	<option value="DC">District Of Columbia</option>
+              	<option value="FL">Florida</option>
+              	<option value="GA">Georgia</option>
+              	<option value="HI">Hawaii</option>
+              	<option value="ID">Idaho</option>
+              	<option value="IL">Illinois</option>
+              	<option value="IN">Indiana</option>
+              	<option value="IA">Iowa</option>
+              	<option value="KS">Kansas</option>
+              	<option value="KY">Kentucky</option>
+              	<option value="LA">Louisiana</option>
+              	<option value="ME">Maine</option>
+              	<option value="MD">Maryland</option>
+              	<option value="MA">Massachusetts</option>
+              	<option value="MI">Michigan</option>
+              	<option value="MN">Minnesota</option>
+              	<option value="MS">Mississippi</option>
+              	<option value="MO">Missouri</option>
+              	<option value="MT">Montana</option>
+              	<option value="NE">Nebraska</option>
+              	<option value="NV">Nevada</option>
+              	<option value="NH">New Hampshire</option>
+              	<option value="NJ">New Jersey</option>
+              	<option value="NM">New Mexico</option>
+              	<option value="NY">New York</option>
+              	<option value="NC">North Carolina</option>
+              	<option value="ND">North Dakota</option>
+              	<option value="OH">Ohio</option>
+              	<option value="OK">Oklahoma</option>
+              	<option value="OR">Oregon</option>
+              	<option value="PA">Pennsylvania</option>
+              	<option value="RI">Rhode Island</option>
+              	<option value="SC">South Carolina</option>
+              	<option value="SD">South Dakota</option>
+              	<option value="TN">Tennessee</option>
+              	<option value="TX">Texas</option>
+              	<option value="UT">Utah</option>
+              	<option value="VT">Vermont</option>
+              	<option value="VA">Virginia</option>
+              	<option value="WA">Washington</option>
+              	<option value="WV">West Virginia</option>
+              	<option value="WI">Wisconsin</option>
+              	<option value="WY">Wyoming</option>
+              </select>
+          </label>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="suppliers__section-title make__headline"><h3>Seed Suppliers</h3></div>
+
+<div class="suppliers__ctn"><?php $suppliers = new WP_Query( array( 'post_type' => 'suppliers' ) );?>
+    <?php if( $suppliers->have_posts()): ?>
+      <div class="row">
+        <?php while ( $suppliers->have_posts()) : $suppliers->the_post();  ?>
+
+              <div class="col-xs-12 col-sm-4">
+                <div class="box">
+                  <div class="supplier__info">
+                    <h4><?php the_field('supplier_name'); ?></h4>
+                    <span class="supplier__address-copy"><?php the_field('supplier_address'); ?></span>
+                    <span class="supplier__phonenumber-copy"><?php the_field('supplier_phone_number'); ?></span>
+                    <span><a href="#" class="supplier__email-copy"><?php the_field('supplier_email'); ?></a></span>
+                    <span><a href="#" class="supplier__suteurl-copy"><?php the_field('supplier_site_url'); ?></a></span>
+                  </div>
+                </div>
+              </div>
+
+        <?php endwhile; ?>
+      </div>
+    <?php endif ?>
+</div>
+
+<?php get_footer();?>
+ 
