5fb3be418670c3136df77a1ca09f008b4a28f680
Omar Rasool <orasool@hlkagency.com>
2016-08-22 13:38:02 -0500

    adds wp query for seed suppliers and west bread representatives

--- wp-content/themes/connectIN/page-find-seed-supplier.php
+++ wp-content/themes/connectIN/page-find-seed-supplier.php
@@ -81,12 +81,13 @@
 
 <div class="suppliers__section-title make__headline"><h3>Seed Suppliers</h3></div>
 
-<div class="suppliers__ctn"><?php $suppliers = new WP_Query( array( 'post_type' => 'suppliers' ) );?>
+<div class="suppliers__ctn">
+  <?php $suppliers = new WP_Query( array( 'post_type' => 'suppliers' ) );?>
     <?php if( $suppliers->have_posts()): ?>
       <div class="row">
         <?php while ( $suppliers->have_posts()) : $suppliers->the_post();  ?>
 
-              <div class="col-xs-12 col-sm-4">
+              <div value="<?php the_field('supplier_name'); ?>" class="col-xs-12 col-sm-4">
                 <div class="box">
                   <div class="supplier__info">
                     <h4><?php the_field('supplier_name'); ?></h4>
@@ -101,6 +102,31 @@
         <?php endwhile; ?>
       </div>
     <?php endif ?>
+</div>
+
+<div class="rep__ctn">
+<?php $reps = new WP_Query( array( 'post_type' => 'reps' ) );?>
+<h3 class="make__headline">Westbred Representatives</h3>
+<?php if( $reps->have_posts()): ?>
+<div class="row">
+  <?php while ( $reps->have_posts()) : $reps->the_post();  ?>
+  <div class="col-xs-12 col-sm-6">
+    <div class="box">
+      <div class="rep__info">
+        <div class="row">
+        <img src="<?php the_field('rep_photo'); ?>" alt="" />
+        <div class="col-xs-6 col-sm-6"><h4><?php the_field('rep_name') ?></h4>
+        <span><?php the_field('rep_position'); ?></span>
+        <span><?php the_field('rep_phone'); ?></span>
+        <span><?php the_field('rep_email'); ?></span>
+        <span><?php the_field('rep_address'); ?></span></div>
+      </div>
+      </div>
+    </div>
+  </div>
+  <?php endwhile; ?>
+</div>
+<?php endif ?>
 </div>
 
 <?php get_footer();?>
