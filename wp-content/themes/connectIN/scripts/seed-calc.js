4abd8fa7c977769345e10d18a47a5a1162c0ab70
Omar Rasool <orasool@hlkagency.com>
2016-09-19 20:06:04 -0500

    validates email in pdf modal

--- wp-content/themes/connectIN/scripts/seed-calc.js
+++ wp-content/themes/connectIN/scripts/seed-calc.js
@@ -18,6 +18,7 @@ $("#emailDataForm").bind("keypress", function(e) {
    }
 });
 
+
 $('#downloadPDF').click(function (e) {
 	e.preventDefault()
 
@@ -26,55 +27,74 @@ $('#downloadPDF').click(function (e) {
 })
 
 $('#mailPDF').click(function (e) {
-	var queryStringAdd = '&recipients=' + encodeURIComponent($('#recipientEmail').val())
-											+ '&sender=' + encodeURIComponent('no-reply@hlkagency.com')
-											+ '&subject=' + encodeURIComponent('Wheat Profitability Results')
-											+ '&firstName='
-											+ '&memberBusname='
+	//Validate Email
+	function is_email(email){
+	var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+	return emailReg.test(email); }
 
-	$.ajax({
-		url: 'http://hlk-pdf-server.centralus.cloudapp.azure.com/api/v1/Email?templateName=WestBred_ProfitCalc' + queryStringAdd,
-		type: 'POST',
-		data: '{ "json" : ' + JSON.stringify(dataExtract()) + '}',
-		beforeSend: function() {
-			var opts = {
-			  lines: 13 // The number of lines to draw
-			, length: 28 // The length of each line
-			, width: 14 // The line thickness
-			, radius: 42 // The radius of the inner circle
-			, scale: 0.15 // Scales overall size of the spinner
-			, corners: 0.3 // Corner roundness (0..1)
-			, color: '#fff' // #rgb or #rrggbb or array of colors
-			, opacity: 0 // Opacity of the lines
-			, rotate: 0 // The rotation offset
-			, direction: 1 // 1: clockwise, -1: counterclockwise
-			, speed: 1 // Rounds per second
-			, trail: 85 // Afterglow percentage
-			, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-			, zIndex: 2e9 // The z-index (defaults to 2000000000)
-			, className: 'spinner' // The CSS class to assign to the spinner
-			, top: '-20px' // Top position relative to parent
-			, left: '50%' // Left position relative to parent
-			, shadow: false // Whether to render a shadow
-			, hwaccel: false // Whether to use hardware acceleration
-			, position: 'relative' // Element positioning
+	var emailInput = is_email($('#recipientEmail').val())
+	var emailError = '<small class="emailError">Please enter valid email.</small'
+
+	if (emailInput == false) {
+		$('#recipientEmail').css({"border-color": "red"})
+		$('#mailPDF').after(emailError)
+
+	}else {
+		$('.emailError').remove()
+		$('#recipientEmail').css({"border-color": "inherit"})
+		var queryStringAdd = '&recipients=' + encodeURIComponent($('#recipientEmail').val())
+												+ '&sender=' + encodeURIComponent('no-reply@hlkagency.com')
+												+ '&subject=' + encodeURIComponent('Wheat Profitability Results')
+												+ '&firstName='
+												+ '&memberBusname='
+
+		$.ajax({
+			url: 'http://hlk-pdf-server.centralus.cloudapp.azure.com/api/v1/Email?templateName=WestBred_ProfitCalc' + queryStringAdd,
+			type: 'POST',
+			data: '{ "json" : ' + JSON.stringify(dataExtract()) + '}',
+			beforeSend: function() {
+				var opts = {
+				  lines: 13 // The number of lines to draw
+				, length: 28 // The length of each line
+				, width: 14 // The line thickness
+				, radius: 42 // The radius of the inner circle
+				, scale: 0.15 // Scales overall size of the spinner
+				, corners: 0.3 // Corner roundness (0..1)
+				, color: '#fff' // #rgb or #rrggbb or array of colors
+				, opacity: 0 // Opacity of the lines
+				, rotate: 0 // The rotation offset
+				, direction: 1 // 1: clockwise, -1: counterclockwise
+				, speed: 1 // Rounds per second
+				, trail: 85 // Afterglow percentage
+				, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+				, zIndex: 2e9 // The z-index (defaults to 2000000000)
+				, className: 'spinner' // The CSS class to assign to the spinner
+				, top: '-20px' // Top position relative to parent
+				, left: '50%' // Left position relative to parent
+				, shadow: false // Whether to render a shadow
+				, hwaccel: false // Whether to use hardware acceleration
+				, position: 'relative' // Element positioning
+				}
+				var spinner = new Spinner(opts).spin()
+				$('#mailPDF').css('color', 'transparent');
+				$('#mailPDF').after(spinner.el)
 			}
-			var spinner = new Spinner(opts).spin()
-			$('#mailPDF').css('color', 'transparent');
-			$('#mailPDF').after(spinner.el)
-		}
-	})
-	.done(function() {
-		$('.modal').hide()
-		$('.thankyoumodal').toggleClass('active')
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	})
+		})
+		.done(function() {
+			$('.modal').hide()
+			$('.thankyoumodal').toggleClass('active')
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		})
+	}
+
+
+
 
 })
 
