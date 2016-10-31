6c7625d2b8ec7b0f88a914f3309017404bc4f9bd
Paul Graham <pgraham@hlkagency.com>
2016-10-31 10:57:19 -0500

    initial 404

---
+++ wp-content/themes/connectIN/404.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="inner-hero">
+  <h1 class="inner-hero-headline">Page Not Found - 404</h1>
+</div>
+
+<div class="make__wide">
+  <div class="seed__ctn row">
+    <div class=" col-md-7">
+      <div class="box give__room">
+        <h1 class="seed__headline make__headline">Whoops! That link didn't yield much of anything.</h1>
+        <p>
+          Get more out of every page you view by trying one of these links:
+        </p>
+        <ul>
+          <li><a href="/about-the-system/">About the System</a></li>
+          <li><a href="/optimal-seeding-rate/">Optimal Seeding Rate</a></li>
+          <li><a href="/wheat-profitability-calculator/">Wheat Profitability Calculator</a></li>
+          <li><a href="/find-seed-supplier/">Find a Seed Supplier</a></li>
+          <li><a href="/contact-us/">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
