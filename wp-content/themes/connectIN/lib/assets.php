0acd7cdf1f8c6a422486ce0bafae1e8771abe1e8
Paul Graham <pgraham@hlkagency.com>
2016-10-31 13:35:28 -0500

    bad scoping fix on links

--- wp-content/themes/connectIN/lib/assets.php
+++ wp-content/themes/connectIN/lib/assets.php
@@ -1,6 +1,6 @@
 <?php
 function offset_load_assets () {
   // TODO: refers to dev stylesheet, rename reference to .min.css for production
-  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', array(), 'v3' );
+  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', array(), 'v4' );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_assets' );
