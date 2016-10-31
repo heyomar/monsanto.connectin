460e860297c0d3df9f08570cefb18eaf1b50ff7a
Paul Graham <pgraham@hlkagency.com>
2016-10-31 13:28:54 -0500

    c-c-c-cachebreaker

--- wp-content/themes/connectIN/lib/assets.php
+++ wp-content/themes/connectIN/lib/assets.php
@@ -1,6 +1,6 @@
 <?php
 function offset_load_assets () {
   // TODO: refers to dev stylesheet, rename reference to .min.css for production
-  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', false );
+  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', array(), 'v3' );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_assets' );
