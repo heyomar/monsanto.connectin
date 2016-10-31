736ccd5c07061fdfc72f03c9ab19349dfc657138
Paul Graham <pgraham@hlkagency.com>
2016-10-31 13:42:57 -0500

    whoops, added compiled

--- wp-content/themes/connectIN/lib/assets.php
+++ wp-content/themes/connectIN/lib/assets.php
@@ -1,6 +1,6 @@
 <?php
 function offset_load_assets () {
   // TODO: refers to dev stylesheet, rename reference to .min.css for production
-  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', array(), 'v4' );
+  wp_enqueue_style( 'master', get_stylesheet_directory_uri() . '/bundle.css', array(), 'v5' );
 }
 add_action( 'wp_enqueue_scripts', 'offset_load_assets' );
