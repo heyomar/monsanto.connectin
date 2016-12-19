1d1deb3123a59f385ddcd7a291843ab2d2cd9d32
Omar Rasool <orasool@hlkagency.com>
2016-12-19 16:20:11 -0600

    how to order page almost done

--- wp-content/themes/connectIN/templates/template-how-to-order.php
+++ wp-content/themes/connectIN/templates/template-how-to-order.php
@@ -8,29 +8,159 @@
 
  <?php get_header(); ?>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+<!--[––––
+			↓ HERO ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
 		<div class="hero">
 			<div class="inner">
 				<div class="row">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="content">
 							<h2 class="title">How To Order The Conenctin System</h2>
-							<p class="copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis eaque, nisi at quibusdam. Obcaecati voluptas quo, eligendi inventore! Fugit magni vel facilis molestias illum at quam necessitatibus quibusdam nemo impedit.</p>
+							<p class="copy">Lorem ipsum dolor sit amet, consectetur adipisicing
+							elit. Facilis eaque, nisi at quibusdam. Obcaecati voluptas quo,
+							eligendi inventore! Fugit magni vel facilis molestias illum at
+							quam necessitatibus quibusdam nemo impedit.</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-
-		<div class="step-one">
-			<div class="inner">
+<!--[––––
+			↓ STEPS ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+		<div class="step step-one">
+			<div class="inner mxw-1000-center">
+				<span class="number">1</span>
 				<div class="content">
-					<span class="title">Purchase the ConnectIN Seed Counter Directly</span>
-					<p class="copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, aperiam ab ipsam, porro dolorum dolor nisi a suscipit molestias fugit consequuntur architecto pariatur maiores voluptates</p>
+					<h3 class="title">Purchase the ConnectIN Seed Counter Directly</h3>
+					<p class="copy">Lorem ipsum dolor sit amet, consectetur adipisicing
+					elit. Nesciunt, aperiam ab ipsam, porro dolorum dolor nisi a suscipit
+					molestias fugit consequuntur architecto pariatur maiores voluptates
+				</p>
 				</div>
 			</div>
 		</div>
 
+		<div class="step step-two">
+			<div class="inner mxw-1000-center">
+				<span class="number">2</span>
+				<div class="content">
+					<h3 class="title">Purchase the ConnectIN Seed Counter Directly</h3>
+					<p class="copy">Lorem ipsum dolor sit amet, consectetur adipisicing
+					elit. Nesciunt, aperiam ab ipsam, porro dolorum dolor nisi a suscipit
+					molestias fugit consequuntur architecto pariatur maiores voluptates
+				</p>
+				</div>
+			</div>
+		</div>
+<!--[––––
+			↓ SUPPORTED DEVICES ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+		<div class="supported-devices">
+			<div class="inner mxw-1000-center">
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-6 col-md-6 nopad">
+						<div class="content tablets">
+							<h3 class="title">Mobile Tablets</h3>
+							<div class="list">
+								<ul>
+									<strong class="list-title">Apple iPad Air 2</strong>
+									<li>IOS 8.0 or greater</li>
+									<li>16GB or greater</li>
+									<li>WiFi or WiFi + Cellular</li>
+									<li>Bluetooth Technology</li>
+								</ul>
+								<ul>
+									<strong class="list-title">Samsung&reg; Galaxy Tab S2 9.7"</strong>
+									<li>Android 5 Lollipop or greater</li>
+									<li>16GB or greater</li>
+									<li>WiFi</li>
+									<li>Bluetooth Technology</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-6 col-md-6 nopad">
+						<div class="content printers">
+							<h3 class="title">Printers</h3>
+							<div class="list">
+								<ul>
+									<strong class="list-title">Brother HL5200DWT or HP LaserJet Pro 400 M402N</strong>
+									<li>Black and White</li>
+									<li>Double Sided</li>
+									<li>Two Trays</li>
+									<li>Wifi Technology</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+<!--[––––
+			↓ COPY SNIPPET ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+		<div class="copy-snippet">
+			<div class="inner mxw-1000-center">
+				<div class="col-xs-12 col-sm-12 col-md-12">
+					<div class="content">
+						<p>If you don’t already own a complementary tablet or printer, you may purchase<br> one using one of the options below.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<div class="find-tablet">
+		<div class="inner">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-4 nopad">
+					<div class="content">
+						<h3 class="title">Live Phone Support<br>and Direct Ordering</h3>
+						<div class="instructions">A preferred electronic provider, Zones™ Inc. is providing for Wheat Seed Suppliers a live ONE on ONE phone support, that can walk you through and provide direct information of hardware specifications to purchase tablets or printers.
+						Call Zones today at (253) 205-3798 to order your tablet or printer.</div>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-4 nopad">
+					<div class="content amazon">
+						<h3 class="title">Amazon.com Wish List<br>for System Hardware</h3>
+						<div class="instructions">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero aut incidunt, soluta dolorum autem optio, sequi. Molestiae temporibus voluptates quibusdam voluptatem, nemo porro repellat rem molestias non est. Id, labore. <br><br>
+						<a class="button" href="">Shop now</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-4 nopad">
+					<div class="content">
+						<h3 class="title">Purchase Through Any<br>Electronic Retailer<sup>**</sup></h3>
+						<div class="instructions">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, velit, quibusdam eum consequuntur illum vel incidunt, perferendis ratione quo alias mollitia, obcaecati odio nulla commodi magni fugit accusamus unde! Praesentium?
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+<!--[––––
+			↓ INCENTIVES ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+	<div class="incentives">
+		<div class="inner mxw-1000-center">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12">
+					<div class="content">
+						<h3 class="title">Program Incentives</h3>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, exercitationem cupiditate aspernatur sint saepe autem nesciunt, veritatis iure rerum suscipit excepturi aliquid assumenda laudantium magnam quos, tempora libero soluta, nam. <br><br>
+						<a class="button" href="">Learn More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<?php endwhile; ?>
 <?php endif ?>
