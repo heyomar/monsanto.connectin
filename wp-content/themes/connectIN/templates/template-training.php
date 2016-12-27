2ecc5886cf1469f64b2bd5533e7ffa620dd88a1d
Omar Rasool <orasool@hlkagency.com>
2016-12-27 15:57:56 -0600

    act tags for training content

--- wp-content/themes/connectIN/templates/template-training.php
+++ wp-content/themes/connectIN/templates/template-training.php
@@ -150,13 +150,117 @@
 					<div class="col-xs-12">
 						<div class="content">
 							<h2 class="title">Quick Training Tips</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias eligendi illo exercitationem explicabo harum beatae nesciunt, cupiditate, fugit accusantium reprehenderit atque dolorum placeat minus enim, ducimus possimus voluptatibus, perspiciatis libero.</p>
+							<?php the_field('qtt_copy'); ?>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 
+		<!--[––––
+					↓ QUICK TIP - MOBILE VIDEO PLAYER ↓
+		–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+				<div class="carousel">
+					<div class="inner nopad">
+						<div class="row">
+							<div class="col-xs-12">
+								<div id="slick-reporting" class="content">
+
+								<?php if( have_rows('quick_tip_videos') ): ?>
+									<?php while ( have_rows('quick_tip_videos') ) : the_row(); ?>
+
+									<div class="block">
+										<h4><?php the_sub_field('video_title') ?></h4>
+										<a href="<?php
+										$iframe = get_sub_field('video');
+										preg_match('/src="(.+?)"/', $iframe, $matches);
+										$src = $matches[1];
+										echo $src;?>" data-lity> <div style="background-image:url('http://placehold.it/350x150');" class="thumbnail"></div> </a>
+									</div>
+
+									<?php endwhile; ?>
+								<?php endif; ?>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+		<!--[––––
+					↓ QUICK TIP - DESKTOP VIDEO PLAYER ↓
+		–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+				<div class="player">
+					<div class="inner">
+						<div class="row">
+							<div class="col-xs-12 col-sm-4 col-md-4 nopad">
+								<div class="content">
+									<div class="videos">
+										<ul class="list">
+											<?php if( have_rows('quick_tip_videos') ): ?>
+												<?php while ( have_rows('quick_tip_videos') ) : the_row(); ?>
+
+													<?php
+													$iframe = get_sub_field('video');
+													preg_match('/src="(.+?)"/', $iframe, $matches);
+													$src = $matches[1];
+
+													echo '<li data-video="' . $src . '" class="item">' . get_sub_field('video_title') . '</li>';
+													?>
+
+												<?php endwhile; ?>
+											<?php endif; ?>
+										</ul>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-xs-12 col-sm-8 col-md-8 nopad">
+								<div class="content">
+									<div class="video">
+										<iframe id="frame" width="560" height="315" src="https://www.youtube.com/embed/6DBi41reeF0" frameborder="0" allowfullscreen></iframe>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+<!--[––––
+			↓ DOWNLOAD TRAINING MANUALS ↓
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
+	<div class="download-manuals">
+		<div class="inner mxw-1000-center">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="content">
+						<h2 class="title">Download Full Training Manuals</h2>
+						<?php if( have_rows('full_training_manual') ): ?>
+
+						    <?php while ( have_rows('full_training_manual') ) : the_row(); ?>
+									<div class="action">
+										<div class="mxw-600-center">
+											<div class="row">
+												<div class="col-xs-11 col-sm-9 nopad">
+													<div class="download"><a href="<?php the_sub_field('file') ?>"><?php the_sub_field('file_name'); ?></a></div>
+												</div>
+
+												<div class="col-xs-1 col-sm-3 nopad">
+													<div class="email"><a href="#">EMAIL</a></div>
+												</div>
+											</div>
+										</div>
+									</div>
+
+						    <?php endwhile; ?>
+
+						<?php else : ?>
+
+						<?php endif; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<?php endwhile; ?>
 <?php endif ?>
 <?php get_footer(); ?>
