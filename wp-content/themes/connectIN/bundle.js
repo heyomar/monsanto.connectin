5f25a0bab850b4278c03e5ab57c389ab02d66549
Paul Graham <pgraham@hlkagency.com>
2016-12-27 23:23:31 -0600

    fixes fragile geoloc?

--- wp-content/themes/connectIN/bundle.js
+++ wp-content/themes/connectIN/bundle.js
@@ -1862,51 +1862,52 @@ if ($('body').hasClass('contact-us')) {
 
 
 	}
-  if( $('body').hasClass('find-seed-supplier') ) {
-    $('#stateselect').change(function () {
-      changeState()
-    })
+if( $('body').hasClass('find-seed-supplier') ) {
+  $('#stateselect').change(function () {
+    changeState()
+  })
 
-    function changeState () {
-      if ($('#results').hasClass('hidden')) {
-        $('#results').removeClass('hidden')
-      }
-      var selectedstate = $('#stateselect option:selected').val()
-      $('.supplier, .rep').hide()
-      $('.' + selectedstate).show()
-
-      if (!$('.' + selectedstate)[0]) {
-        if ($('#stateselect option:selected').text() === 'Select a state') {
-          $('#results').hide()
-        }else {
-          $('#results').show()
-          $('.failure__nosuppliers').show()
-          var stateChosen = $('#stateselect option:selected').text()
-          $('.failureSpan').text(stateChosen)
-          $('.rep__ctn').hide()
-        }
-      } else {
-          $('.failure__nosuppliers').hide()
-          $('.rep__ctn').show()
-      }
+  function changeState () {
+    if ($('#results').hasClass('hidden')) {
+      $('#results').removeClass('hidden')
     }
+    var selectedstate = $('#stateselect option:selected').val()
+    $('.supplier').hide()
+    $('.' + selectedstate).show()
 
-    navigator.geolocation.getCurrentPosition(success, error)
-
-    function success (position) {
-      var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyAIapQbBrBcIFTuIlMxbXbMty3dT7R1b2k'
-
-      $.getJSON(GEOCODING).done(function (location) {
-        var thestate = location.results[0].address_components[4].short_name
-        $('#stateselect').val(thestate)
-        changeState()
-      })
-    }
-
-    function error (err) {
-      console.log(err)
+    if (!$('.' + selectedstate)[0]) {
+      if ($('#stateselect option:selected').text() === 'Select a state') {
+        $('#results').hide()
+      }else {
+        $('#results').show()
+        $('.failure__nosuppliers').show()
+        var stateChosen = $('#stateselect option:selected').text()
+        $('.failureSpan').text(stateChosen || "your state")
+        // $('.rep__ctn').hide()
+      }
+    } else {
+        $('.failure__nosuppliers').hide()
+        // $('.rep__ctn').show()
     }
   }
+
+  navigator.geolocation.getCurrentPosition(success, error)
+
+  function success (position) {
+    console.log(position.coords.latitude)
+    var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyAIapQbBrBcIFTuIlMxbXbMty3dT7R1b2k'
+
+    $.getJSON(GEOCODING).done(function (location) {
+      var thestate = location.results[6].address_components[0].short_name
+      $('#stateselect').val(thestate)
+      changeState()
+    })
+  }
+
+  function error (err) {
+    console.log(err)
+  }
+}
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
