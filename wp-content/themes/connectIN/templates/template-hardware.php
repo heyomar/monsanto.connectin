f059b4b4cea840f9e794adf5b4b036ad069602da
Omar Rasool <orasool@hlkagency.com>
2017-01-09 11:45:21 -0600

    changes hardware page layout

--- wp-content/themes/connectIN/templates/template-hardware.php
+++ wp-content/themes/connectIN/templates/template-hardware.php
@@ -13,7 +13,7 @@
 			↓ INTRO HERO ↓
 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
 			<section class="intro">
-				<div class="inner">
+				<div class="inner mxw-1250-center">
 					<div class="row">
 							<div class="col-xs-12 col-sm-12 col-md-6">
 								<div class="content">
@@ -40,18 +40,18 @@
 <!--[––––
 			↓ CALLOUTS ↓
 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
-			<section class="callouts">
+			<section class="callouts mxw-1250-center">
 					<div class="row">
-						<div class="col-xs-12 col-sm-6 nopad">
-							<div class="content order">
+						<div class="col-xs-12 col-sm-6 nopad order">
+							<div class="content ">
 								<h2><?php the_field('ots_title'); ?></h2>
 								<div class="copy"><?php the_field('ots_copy'); ?></div>
 								<a href="<?php the_field('ots_button_link'); ?>" class="button"><?php the_field('ots_button_copy'); ?></a>
 							</div>
 						</div>
 
-						<div class="col-xs-12 col-sm-6 nopad">
-							<div class="content training">
+						<div class="col-xs-12 col-sm-6 nopad training">
+							<div class="content">
 								<h2><?php the_field('tm_title'); ?></h2>
 								<div class="copy"><?php the_field('tm_copy'); ?></div>
 								<a href="<?php the_field('tm_button_link'); ?>" class="button"><?php the_field('tm_button_copy'); ?></a>
@@ -62,21 +62,18 @@
 <!--[––––
 			↓ BENEFITS ↓
 –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––//]-->
-			<section class="benefits">
+			<section class="benefits mxw-1000-center">
 				<div class="lead">
 					<div class="row">
-						<div class="col-xs-12 col-sm-12 col-md-6">
+						<div class="col-xs-12 col-sm-12 col-md-12">
 							<div class="content">
-								<h2 class="title"><?php the_field('sb_title'); ?></h2>
-								<p class="copy"><?php the_field('sb_copy'); ?></p>
+								<h2 class="title center-text"><?php the_field('sb_title'); ?></h2>
+								<div class="copy responsive-text-align"><?php the_field('sb_copy'); ?></div>
 							</div>
 						</div>
-
-						<div class="col-xs-12 col-sm-12 col-md-6">
-							<div class="content">
-								<iframe width="560" height="315" src="<?php the_field('sb_video_link'); ?>" frameborder="0" allowfullscreen></iframe>
-							</div>
-						</div>
+					</div>
+					<div class="single-video">
+							<iframe width="560" height="315" src="<?php the_field('sb_video_link'); ?>" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
 <!--[––––
